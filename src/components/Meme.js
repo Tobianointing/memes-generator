a6dbee8db98d0d7f3b5c8a24e50bea9aca671034
Tobianointing <adeyokunnuo@gmail.com>
2022-05-15 15:52:44 +0100

    bless

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,10 +2,10 @@ export default function Meme(props) {
   return (
     <section className="meme--wrapper">
       <div className="meme-img--wrapper">
-        <h1 className="up-text">{props.data.upText}</h1>
+        <h1 className="meme--text up-text">{props.data.upText}</h1>
         <img src={props.randomMeme} alt="" />
-        <h1 className="down-text">{props.data.downText}</h1>
+        <h1 className="meme--text down-text">{props.data.downText}</h1>
       </div>
     </section>
-  );
+  )
 }
